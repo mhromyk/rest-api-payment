63fffff874cd889bdf7ec29374d868f3b11fb920
Maks <mhromyk.edu@gmail.com>
2023-12-08 16:04:19 +0100

    initial empty project commit

---
+++ src/test/java/mh/springframework/restapi/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.springframework.restapi.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
